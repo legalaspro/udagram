fe829fdad47ff462b9d354389b35e176fd991c48
Dmitri Manajev <dmitri.manajev@protonmail.com>
2023-07-19 13:01:42 +0200

    Updated circle ci config and deploy script for udagram frontend

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,12 @@
-aws s3 cp --recursive --acl public-read ./www s3://myawsbucket-75139724085/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://myawsbucket-75139724085/
+
+# Ensure AWS_BUCKET environment variable is set
+if [ -z "$AWS_BUCKET" ]; then
+  echo "AWS_BUCKET environment variable is not set."
+  exit 1
+fi
+
+# Now you can use the AWS_BUCKET environment variable in your commands
+echo "Deploying to AWS Bucket: $AWS_BUCKET"
+
+aws s3 cp --recursive --acl public-read ./www s3://$AWS_BUCKET/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://$AWS_BUCKET/
